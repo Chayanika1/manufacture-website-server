0e49911e245f24d338bd6cbd70fe3bd19c45ae77
chayanika chakraborty <2016chayanika@gmail.com>
2022-05-25 11:42:35 +0530

    added stripe

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require ('express');
+const express = require('express');
 const cors = require('cors');
-require ('dotenv').config();
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //middle ware
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ngj4j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -16,80 +18,97 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
-  try {
-    await client.connect();
-    const toolCollection = client.db('tools').collection('collection');
-    const bookingCollection = client.db('tools').collection('bookings');
-    const customerCollection = client.db('tools').collection('allbook');
-    const personCollection = client.db('tools').collection('person');
-    
-    app.get("/data" ,async(req,res)=>{
-      const query = {};
-      const cursor = toolCollection.find(query);
-      const items = await cursor.toArray();
-      res.send(items);
+    try {
+        await client.connect();
+        const toolCollection = client.db('tools').collection('collection');
+        const bookingCollection = client.db('tools').collection('bookings');
+        const customerCollection = client.db('tools').collection('allbook');
+        const personCollection = client.db('tools').collection('person');
 
-    })
-    app.get("/review" ,async(req,res)=>{
-      const query = {};
-      const cursor = customerCollection.find(query);
-      const items = await cursor.toArray();
-      res.send(items);
+        app.get("/data", async (req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
 
-    })
-    app.get("/data/:id" ,async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id:ObjectId(id)};
-      const result = await toolCollection.findOne(query);
-      res.send(result)
-    })
-    app.post('/booking',async(req,res)=>{
-      const book = req.body;
-      const query = {booking:book.booking, buyer:book.buyer,name: book.name,price:book.price}
-      const exists = await bookingCollection.findOne(query);
-      if(exists){
-        return res.send ({success:false, book:exists})
-      }
-      const result = await bookingCollection.insertOne(book);
-      res.send({success:true,result})
-    })
-    app.get('/booking',async(req,res)=>{
-      const buyer = req.query.buyer;
-      const query = {buyer:buyer};
-      const orders = await bookingCollection.find(query).toArray();
-      res.send(orders)
-    })
-    app.get('/booking/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id :ObjectId(id)};
-        const booking = await bookingCollection.findOne(query);
-        res.send(booking)
-    })
-    //post reviews in ui
-    app.post('/review',async(req,res)=>{
-      const newUser = req.body;
-      const result = await customerCollection.insertOne(newUser);
-      res.send(result)
-    })
+        })
+        app.get("/review", async (req, res) => {
+            const query = {};
+            const cursor = customerCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
 
-    
-    
-    //root
-    app.get('/',(req,res)=>{
-      res.send("hello i can learn code");
-    })
-    app.post('/person', async(req,res)=>{
-        const person = req.body;
-        const result = await personCollection.insertOne(person);
-        res.send(result)
-    })
+        })
+        app.get("/data/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolCollection.findOne(query);
+            res.send(result)
+        })
+        app.post('/booking', async (req, res) => {
+            const book = req.body;
+            const query = { booking: book.booking, buyer: book.buyer, name: book.name, price: book.price }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, book: exists })
+            }
+            const result = await bookingCollection.insertOne(book);
+            res.send({ success: true, result })
+        })
+        app.get('/booking', async (req, res) => {
+            const buyer = req.query.buyer;
+            const query = { buyer: buyer };
+            const orders = await bookingCollection.find(query).toArray();
+            res.send(orders)
+        })
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking)
+        })
+        //post reviews in ui
+        app.post('/review', async (req, res) => {
+            const newUser = req.body;
+            const result = await customerCollection.insertOne(newUser);
+            res.send(result)
+        })
+        app.post('/create-payment-intent', async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = parseInt(price) * 100;
+            console.log("amount", amount);
+            if (!isNaN(amount)) {
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: amount,
+                    currency: 'usd',
+                    payment_method_types: ['card']
+                });
+                res.send({ clientSecret: paymentIntent.client_secret })
 
-  }
-  finally {
-    //await client.close()
+            }
 
 
-  }
+        });
+
+
+
+        //root
+        app.get('/', (req, res) => {
+            res.send("hello i can learn code");
+        })
+        app.post('/person', async (req, res) => {
+            const person = req.body;
+            const result = await personCollection.insertOne(person);
+            res.send(result)
+        })
+
+    }
+    finally {
+        //await client.close()
+
+
+    }
 }
 run().catch(console.dir);
 
@@ -97,6 +116,6 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-  console.log('singing', port)
+    console.log('singing', port)
 })
 
